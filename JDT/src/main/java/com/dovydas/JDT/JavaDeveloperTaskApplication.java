08147520cadce0ad18819eee388a99025deacae9
Layonas <dovdaszl@gmail.com>
2022-06-03 13:48:34 +0300

    Created initial project

---
+++ JDT/src/main/java/com/dovydas/JDT/JavaDeveloperTaskApplication.java
@@ -0,0 +1,13 @@
+package com.dovydas.JDT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDeveloperTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDeveloperTaskApplication.class, args);
+	}
+
+}
---
+++ JDT/src/test/java/com/dovydas/JDT/JavaDeveloperTaskApplicationTests.java
@@ -0,0 +1,14 @@
+package com.dovydas.JDT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDeveloperTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
